319084efe06cac8bdb7cc79496a33766b37afeaa
fullt1lt <daniiltupikin98@gmail.com>
2026-02-28 13:29:29 +0100

    Lesson25

--- beta_1/my_phone/urls.py
+++ beta_1/my_phone/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, hello, helloid
+from .views import home, hello, helloid, product_detail
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path("home/", home, name="home"),
     path("hello/<str:name>", hello, name="hello_str"),
     path("helloid/<int:id>", helloid, name="hello_id"),
+    path("product/<int:id>", product_detail, name="product_detail"),
 ]
 
 if settings.DEBUG:
--- beta_1/my_phone/views.py
+++ beta_1/my_phone/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def home(request):
     user_list = ["Alex", "Mark", "Ivan"]
     isAdmin = True
-    products = Product.objects.filter(price__lte=25000)
+    products = Product.objects.exclude(price__range=[20000, 35000]).exclude(
+        category__name="Планшеты"
+    )
     context = {
         "users": user_list,
         "isAdmin": isAdmin,
         "products": products
         }
     return render(request, "home.html", context)
+
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        product = None
+    context = {
+        "product": product
+    }
+    return render(request, "product_detail.html", context)
 
 def hello(request, name):
     name = name.upper()
